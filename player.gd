d507d4aec12d9c038d6cb071cd0a21044b13df4d
Nachiten [Desktop] <ibaptista@frba.utn.edu.ar>
2024-05-04 11:14:24 -0300

    45:40 - Added pine_tree scene

--- player.gd
+++ player.gd
@@ -6,3 +6,8 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * move_velocity
 	move_and_slide()
+
+	if velocity.length() > 0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
